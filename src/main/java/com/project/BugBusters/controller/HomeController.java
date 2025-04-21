a48c6058c83e5ae0dec3f3cba823b830d3bfd726
Suraj Mondal <sm.surojmondal@gmail.com>
2025-04-21 21:14:20 +0530

    Backend

--- src/main/java/com/project/BugBusters/controller/HomeController.java
+++ src/main/java/com/project/BugBusters/controller/HomeController.java
@@ -1,8 +1,10 @@
 package com.project.BugBusters.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Controller
+@CrossOrigin(origins = "*")
 public class HomeController {
 
     public String home(){
